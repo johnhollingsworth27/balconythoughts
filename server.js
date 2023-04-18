b281577d7e5918b3363cf4ea90dc748d0230981a
John  Hollingsworth <jahollingsworth27@gmail.com>
2023-04-18 02:17:06 -0400

    errors

--- server.js
+++ server.js
@@ -11,12 +11,18 @@ const pool = mysql.createPool({
 });
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 app.get('/entries', async (req, res) => {
   try {
